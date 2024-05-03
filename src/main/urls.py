0e6ded4b289f100ec6f30b347274787ede62a207
Joseph Gichohi <josephgichohi322@gmail.com>
2024-05-03 18:00:16 +0300

    urls,supervisors dashboard

--- src/main/urls.py
+++ src/main/urls.py
@@ -6,5 +6,5 @@ from .views import main_view, home_view,dashboard_view
 urlpatterns = [
     path('', main_view, name= 'main'),
     path('home/', home_view, name = 'home'),
-    path('supervisor/', dashboard_view, name='supervisor')
+    path('dashboard/', dashboard_view, name='dashboard')
 ]
